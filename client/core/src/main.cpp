82e1e69d9f880cfb4994e82069bfd5a97745a6e8
sion <i.solomon18@hotmail.com>
2025-07-07 23:07:05 +0300

    FEAT: add basic project setup

---
+++ client/core/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "HELLO FROM CLIENT" << std::endl;
+    return 0;
+}
---
+++ server/core/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "HELLO FROM SERVER" << std::endl;
+    return 0;
+}
