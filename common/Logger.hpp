ef9566672cfdf4e24d1a13e3477d53fe12d725bf
sion <i.solomon18@hotmail.com>
2025-07-21 01:14:24 +0300

    FEAT: clean logging

--- common/Logger.hpp
+++ common/Logger.hpp
@@ -1,16 +1,11 @@
-// Logger.hpp
 #pragma once
 
-#include <iostream>
-#include <mutex>
 #include <cstdarg>
-#include <cstdio>
-#include <string>
 #include <cstring>
 
 namespace Logger
 {
-    /// Returns just the filename portion of a full path.
+    /// Get just the filename
     inline const char *getFilename(const char *path)
     {
         const char *slash = std::strrchr(path, '/');
@@ -18,73 +13,8 @@ namespace Logger
         const char *ptr = (slash > back ? slash : back);
         return ptr ? ptr + 1 : path;
     }
-
-    enum class Level
-    {
-        Error,
-        Info,
-        Debug
-    };
-
-    // Convert enum ? text
-    inline const char *levelToString(Level lvl)
-    {
-        switch (lvl)
-        {
-        case Level::Error:
-            return "ERROR";
-        case Level::Info:
-            return "INFO";
-        case Level::Debug:
-            return "DEBUG";
-        }
-        return "UNKNOWN";
-    }
-
-    class LoggerImpl
-    {
-    public:
-        static LoggerImpl &instance()
-        {
-            static LoggerImpl s_instance;
-            return s_instance;
-        }
-
-        // core log function: formats the user msg and prints with func name
-        void log(Level level,
-                 const char *func,
-                 const char *fmt,
-                 ...)
-        {
-            std::lock_guard<std::mutex> lock(m_mutex);
-
-            // format user message
-            char message_buf[1024];
-            va_list args;
-            va_start(args, fmt);
-            std::vsnprintf(message_buf, sizeof(message_buf), fmt, args);
-            va_end(args);
-
-            // emit: [LEVEL] func(): message
-            std::cout
-                << "[" << levelToString(level) << "] "
-                << func << "() "
-                << message_buf
-                << "\n";
-        }
-
-    private:
-        LoggerImpl() = default;
-        ~LoggerImpl() = default;
-        LoggerImpl(const LoggerImpl &) = delete;
-        LoggerImpl &operator=(const LoggerImpl &) = delete;
-
-        std::mutex m_mutex;
-    };
-
 } // namespace Logger
 
-// pick the best function?signature macro
 #if defined(__GNUC__) || defined(__clang__)
 #define LOGGER_FUNC __PRETTY_FUNCTION__
 #elif defined(_MSC_VER)
@@ -93,7 +23,6 @@ namespace Logger
 #define LOGGER_FUNC __func__
 #endif
 
-/// Logs an ERROR to stderr with filename:line context.
 #define LOG_ERROR(fmt, ...)                                                   \
     do                                                                        \
     {                                                                         \
@@ -102,7 +31,6 @@ namespace Logger
                      Logger::getFilename(__FILE__), __LINE__, ##__VA_ARGS__); \
     } while (false)
 
-/// Logs an INFO to stdout with filename:line context.
 #define LOG_INFO(fmt, ...)                                                    \
     do                                                                        \
     {                                                                         \
@@ -111,7 +39,6 @@ namespace Logger
                      Logger::getFilename(__FILE__), __LINE__, ##__VA_ARGS__); \
     } while (false)
 
-/// Logs a DEBUG to stdout with filename:line context.
 #define LOG_DEBUG(fmt, ...)                                                   \
     do                                                                        \
     {                                                                         \
